c7a5ef643278a9cc1546d680699a0cd0690511cb
Greg <43392709+gpric024@users.noreply.github.com>
2021-12-19 16:08:43 -0500

    Update pytwitter_tools.py

--- Data_Collection/Script Collection Files/pytwitter_tools.py
+++ Data_Collection/Script Collection Files/pytwitter_tools.py
@@ -32,14 +32,24 @@ depression_phrases = ["i have depression",
 
 
 def authorize():
-    consumer_key = 'X8y5NRfbKvdbfJoeqH5wcwKf9'
-    consumer_key_secret = 'zcoUREqyclv3SePHVJCaiePUzneCSyGf5tZG91n0mVRJdmjlv0'
-    access_token = '1443757769824165889-cdJIn7XoLOLJuCEgMJ0yKbIMYVvxxh'
-    acess_token_secret = 'T18YIqFWG50SowDLZWB0BOOfUKR750YYVBntdU00Smt16'
+    consumer_key = 'c7kjCYQ0gIY60zBP2Ng4kPC7K'  # gpric024 account
+    consumer_key_secret = 'O4vSqiZpb0ccsdEPxub6AUoJovR7eXmHPreE4fUfniMMfzhtAj'
+    access_token = '1437574559121776643-dLFJLlQUi4O72PTfBJlFk7GzudwXAF'
+    acess_token_secret = 'wGDxMnGILcwtdnkdqw7o4AhbtxJr9W8DaOmuVlpCrVecC'
     auth = tw.OAuthHandler(consumer_key, consumer_key_secret)
     auth.set_access_token(access_token, acess_token_secret)
     api = tw.API(auth, wait_on_rate_limit=True)
     return api
+
+# def authorize():
+#     consumer_key = 'X8y5NRfbKvdbfJoeqH5wcwKf9' #UNI or Fil account
+#     consumer_key_secret = 'zcoUREqyclv3SePHVJCaiePUzneCSyGf5tZG91n0mVRJdmjlv0'
+#     access_token = '1443757769824165889-cdJIn7XoLOLJuCEgMJ0yKbIMYVvxxh'
+#     acess_token_secret = 'T18YIqFWG50SowDLZWB0BOOfUKR750YYVBntdU00Smt16'
+#     auth = tw.OAuthHandler(consumer_key, consumer_key_secret)
+#     auth.set_access_token(access_token, acess_token_secret)
+#     api = tw.API(auth, wait_on_rate_limit=True)
+#     return api
 
 
 def phrase_search(api):
@@ -47,7 +57,7 @@ def phrase_search(api):
     Returns user of the last person who tweeted a specific phrase, and the list of fetched tweets
     '''
     fetched_tweets = api.search_tweets("\"i have depression\"", count=1, result_type="recent", lang="en")
-
+    print(fetched_tweets[0]._json['text'])
     anchor_id = fetched_tweets[0]._json['user']['id']
 
     return anchor_id
@@ -60,10 +70,10 @@ def get_friends_list(api, user):
     '''
     anchor_friends_ids = []
     try:
-      anchor_friends_raw = api.get_friends(user_id=user.get_UserID(), count=200)
+        anchor_friends_raw = api.get_friends(user_id=user.get_UserID(), count=200)
     except Exception as e:
-      print(e)
-      return anchor_friends_ids
+        print(e)
+        return anchor_friends_ids
 
     for i in anchor_friends_raw:
         anchor_friends_ids.append(i._json['id'])
@@ -71,25 +81,25 @@ def get_friends_list(api, user):
     return anchor_friends_ids
 
 
-def check_protected(api, userID):
-    '''
-    Check if a twitter acount is set to private by their UserID
-    '''
-    try:
-        user = api.get_user(user_id=userID)
-        if (user._json['protected'] == 'True'):
-            return True
-    except Exception as e:
-        print(e)
-        return False
-    return False
+# def check_protected(api, userID):
+#     '''
+#     Check if a twitter acount is set to private by their UserID
+#     '''
+#     try:
+#         user = api.get_user(user_id=userID)
+#         if user._json['protected'] == 'True':
+#             return True
+#     except Exception as e:
+#         print(e)
+#         return False
+#     return False
 
 
 def get_tweets_by_id(api, userID):
     '''
     Get all tweets from a user by their UserID
     '''
-    if not check_protected(api, userID):
+    try:
         all_tweets = api.user_timeline(user_id=userID,
                                        # 200 is the maximum allowed count
                                        # try setting it to 1000
@@ -100,7 +110,8 @@ def get_tweets_by_id(api, userID):
                                        tweet_mode='extended'
                                        )
         return all_tweets
-    else:
+
+    except Exception as e:
         return []
 
 
@@ -114,7 +125,7 @@ def depression_check_user(api, userID):
 
     for info in tweets:
         for depression_phrase in depression_phrases:
-            if (depression_phrase in info.full_text.lower()):
+            if depression_phrase in info.full_text.lower():
                 # print(info.full_text)
                 depression_status = True
 
@@ -122,6 +133,7 @@ def depression_check_user(api, userID):
 
 
 def create_Set_And_Graph(api, user, G, allUsers):
+
     # lvl1_friends is a list of USER IDS
     lvl1_friends = get_friends_list(api, user)
 
@@ -132,20 +144,19 @@ def create_Set_And_Graph(api, user, G, allUsers):
         print(newUserID)
         success = False
         # Checking if the new user is protected before instantiating their user id as an object of class User
-        if (check_protected(api, newUserID) == False):
-            try:
-                newUser = tu.User(userID=newUserID,
-                                  depression_status=depression_check_user(api, newUserID))
-                success = True
-            except Exception as e:
-                print(e)
-                success = False
-            if success:
-                lvl1_users.append(newUser)
-                if newUserID not in allUsers:
-                    allUsers.add(newUserID)
-                    G.add_node(newUser)
-                    G.add_edge(newUser, user)
+        try:
+            newUser = tu.User(userID=newUserID,
+                              depression_status=depression_check_user(api, newUserID))
+            success = True
+        except Exception as e:
+            print(e)
+            success = False
+        if success:
+            lvl1_users.append(newUser)
+            if newUserID not in allUsers:
+                allUsers.add(newUserID)
+                G.add_node(newUser)
+                G.add_edge(newUser, user)
     user.set_followers(lvl1_users)
     return user, G, allUsers
 
@@ -153,14 +164,13 @@ def create_Set_And_Graph(api, user, G, allUsers):
 def get_Outside_Edges(api, G):
     lst_users = []
     for node in G:
-        if (G.degree(node) == 1):
+        if G.degree(node) == 1:
             lst_users.append(node)
     return lst_users
 
 
 def expand_Graph(api, G, allUsers, maxLayers, maxUsers):
     counter = 0
-    currNumberOfUsers = len(allUsers)
     while counter < maxLayers and len(allUsers) < maxUsers:
         print(len(allUsers))
         outside_nodes = get_Outside_Edges(api, G)
@@ -170,30 +180,42 @@ def expand_Graph(api, G, allUsers, maxLayers, maxUsers):
 
 
 def pull_timeline(api, userID):
-    tweets = get_tweets_by_id(api, userID)
     tweet_history = ""
-    for tweet in tweets:
-        tweet_history += (json.dumps(tweet._json) + "\n")
-    return tweet_history
-
+    try:
+        tweets = get_tweets_by_id(api, userID)
+        for tweet in tweets:
+            tweet_history += (json.dumps(tweet._json) + "\n")
+        return tweet_history
+    except Exception as e:
+        return tweet_history
 
 def findNode(userID, G):
     for n in G:
         if userID == n.get_UserID():
             return n
-    return False
+    return None
 
 
 def add_Edges(api, node, G):
-    for n in node.get_followers():
-        friendIds = get_friends_list(api, n)
+    lst = node.get_followers()
+    cnt = 1
+    total = len(lst)
+    for n in lst:
+        print("Friend " + str(cnt) + "/" + str(total))
+        cnt += 1
+        # friendIds = get_friends_list(api, n)
         friend = findNode(n.get_UserID(), G)
-        for iD in friendIds:
-            tmp = findNode(iD, G)
-            if (tmp != False):
-                if not G.has_edge(friend, tmp):
-                    G.add_edge(friend, tmp)
+        if friend is not None:
+            if not G.has_edge(node, friend):
+                G.add_edge(node, friend)
                 print("Connection Added")
+        # for iD in friendIds:
+        #     tmp = findNode(iD, G)
+        #     if tmp != False:
+        #         if not G.has_edge(friend, tmp):
+        #             G.add_edge(friend, tmp)
+        #             print("Connection Added")
+        #         print("Connection already added")
 
 
 def main():
@@ -215,11 +237,13 @@ def main():
     temp, G, allUsers = create_Set_And_Graph(api, anchorUser, G, allUsers)
 
     # Maximum amount of users we want to reach or max Layer depth
-    expand_Graph(api, G, allUsers, 5, 10000)
+    expand_Graph(api, G, allUsers, 1, 1000)
 
     df = pd.DataFrame(columns=['UserID', 'TweetHistory', 'Depression_Status'])
-
+    counter = 1
+    graphSize = G.size()
     for node in G.copy():
+        print(str(counter) + "/" + str(graphSize) + "|| Adding Edges to Node: " + str(node))
         add_Edges(api, node, G)
         my_id = node.get_UserID()
         timeline = pull_timeline(api, my_id)
@@ -227,6 +251,24 @@ def main():
         df2 = pd.DataFrame([[my_id, timeline, depression_stat]],
                            columns=['UserID', 'TweetHistory', 'Depression_Status'])
         df = df.append(df2, ignore_index=True)
+        counter += 1
+
+    counter = 1
+    #Adding outside edges
+    lst = get_Outside_Edges(api,G)
+    for node in lst:
+        print(str(counter)+"/"+str(len(lst)) +" Outside edge")
+        counter+=1
+        try:
+            friendIds = get_friends_list(api, node)
+        except Exception as e:
+            friendIds = []
+        for id in friendIds:
+            friend = findNode(id, G)
+            if friend is not None:
+                if not G.has_edge(node, friend):
+                    G.add_edge(node, friend)
+                    print("Connection Added to outside edge")
 
     nx.write_edgelist(G, path="GeneratedGraph.edgelist")
     df.to_csv("User_Timeline_Label.csv")
